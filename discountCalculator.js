b51b7751008d79f05e7c6f8de4de2d37553c03be
Danielle <danielle.shokrian@aol.com>
2025-07-11 12:13:05 -0400

    Discount calculator using debugger

--- discountCalculator.js
+++ discountCalculator.js
@@ -1,17 +1,20 @@
 function calculateDiscountedPrice(quantity, pricePerItem) {
+    debugger;
     let totalPrice = 0;
-    debugger
-    for (let i = 1; i < quantity; i++) {
+    
+    for (let i = 0; i < quantity; i++) {
         totalPrice += pricePerItem;
+        debugger;
     }
 
     if (quantity >= 10) {
+        debugger;
         totalPrice *= 0.9;
     }
-
+    debugger;
     return totalPrice;
 }
 
-
+calculateDiscountedPrice(10, 5);
 
 module.exports = calculateDiscountedPrice;
